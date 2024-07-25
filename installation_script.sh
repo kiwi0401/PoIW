80ca1eb586b525c287c2befaef9e1dfb0b53826c
Adib Hasan <nzxcvb5@gmail.com>
2024-07-25 16:51:13 -0400

    multi token support and nginx

--- installation_script.sh
+++ installation_script.sh
@@ -12,8 +12,8 @@ source ~/.zshrc
 
 # install vllm
 conda create --name vllm python=3.10
-conda activate vllm
-pip install vllm==0.5.1
+
+conda activate vllm && pip install vllm==0.5.1
 
 # clone repo
 git clone https://github.com/Neehan/PoIW.git
@@ -21,10 +21,19 @@ cd PoIW/
 mkdir data data/llm_cache data/datasets
 echo "HF_HOME=~/PoIW/data/llm_cache/" > .env
 
+sudo apt update
+sudo apt install nginx
+
+# neuralmagic/Meta-Llama-3-70B-Instruct-FP8
+# NousResearch/Meta-LLama-3-70B-Instruct
+
 wget -P data/datasets/ https://huggingface.co/datasets/anon8231489123/ShareGPT_Vicuna_unfiltered/resolve/main/ShareGPT_V3_unfiltered_cleaned_split.json
 python vllm/throughput_benchmark.py \
---model NousResearch/Meta-LLama-3-70B-Instruct \
+--model neuralmagic/Meta-Llama-3-70B-Instruct-FP8 \
 --gpu-memory-utilization 0.98 \
--tp 8 \
+-tp 1 \
 --enforce-eager \
 
+
+# server 
+# python -m vllm.entrypoints.api_server --host 0.0.0.0 --port 8000 --download-dir data/llm_cache/ --model neuralmagic/Meta-Llama-3-70B-Instruct-FP8
