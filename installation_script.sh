8f96a9ce28e10d219accdb8d7eb5f72ea095d06c
Adib Hasan <nzxcvb5@gmail.com>
2024-07-29 13:56:43 -0400

    added ngnix script

--- installation_script.sh
+++ installation_script.sh
@@ -21,19 +21,21 @@ cd PoIW/
 mkdir data data/llm_cache data/datasets
 echo "HF_HOME=~/PoIW/data/llm_cache/" > .env
 
-sudo apt update
-sudo apt install nginx
-
 # neuralmagic/Meta-Llama-3-70B-Instruct-FP8
 # NousResearch/Meta-LLama-3-70B-Instruct
 
 wget -P data/datasets/ https://huggingface.co/datasets/anon8231489123/ShareGPT_Vicuna_unfiltered/resolve/main/ShareGPT_V3_unfiltered_cleaned_split.json
-python vllm/throughput_benchmark.py \
---model neuralmagic/Meta-Llama-3-70B-Instruct-FP8 \
---gpu-memory-utilization 0.98 \
--tp 1 \
---enforce-eager \
+# python vllm/throughput_benchmark.py \
+# --model neuralmagic/Meta-Llama-3-70B-Instruct-FP8 \
+# --gpu-memory-utilization 0.98 \
+# -tp 1 \
+# --enforce-eager \
 
+sudo apt update
+sudo apt install nginx
+mv vllm_server/ngnix.text /etc/ngnix/sites-available/default
+
+# run server on port 9000, ngnix on port 8000 and forward traffic
 
 # server 
 # python -m vllm_server.server --host 127.0.0.1 --port 9000 --download-dir data/llm_cache/ --model neuralmagic/Meta-Llama-3-70B-Instruct-FP8
